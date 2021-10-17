01ef9e87271ca15d5da30ab6b6e8a048ec28e7a4
Tran Thuan <thuant@ames.edu.vn>
2021-10-18 00:30:59 +0700

    Update Learning ReactJS

---
+++ src/Lessons/Advanced/Hooks/UseCallback/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UseCallback = props => {
+  return (
+    <div>
+      <h1> Đây là trang học useCallback !</h1>
+    </div>
+  );
+};
+
+UseCallback.propTypes = {
+
+};
+
+export default UseCallback;
